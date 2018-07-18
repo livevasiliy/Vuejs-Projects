c4dab91031c7fa9f756c14b0899025e24099283a
livevasiliy <vasiliy.mv.obzor@gmail.com>
2018-07-18 22:56:26 +0400

    Добавил пятый проект

---
+++ Exercise 5/app.js
@@ -0,0 +1,22 @@
+new Vue({
+  el: '#exercise',
+  data: {
+    visible: true,
+    array: ['Max', 'Anna', 'Chris', 'Manu'],
+    myObject: {
+      title: 'Lord of the Rings',
+      author: 'J.R.R. Tolkiens',
+      books: '3'
+    },
+    testData: {
+      name: 'TESTOBJECT',
+      id: 10,
+      data: [1.67, 1.33, 0.98, 2.21]
+    }
+  },
+  methods: {
+    toogle: function () {
+      this.visible = !this.visible;
+    }
+  }
+});
