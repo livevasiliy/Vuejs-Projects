3d4d108241cac13fbdc686aa11a303a651c6db73
livevasiliy <vasiliy.mv.obzor@gmail.com>
2018-07-20 15:00:13 +0400

    Добавил новый проект Wonderful Quotes

---
+++ Wonderful Quotes/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <app-header :maxQuotes="maxQuetes" :countQuotes="quetes.length"></app-header>
+        <app-new-quote @quoteAdding="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quetes" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on a Quote to delete it</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import NewQuote from "./components/NewQuote.vue";
+import QuoteGrid from "./components/QuoteGrid.vue";
+export default {
+  data: function() {
+    return {
+      quetes: ["Just a quetes to see something"],
+      maxQuetes: 10
+    };
+  },
+  methods: {
+    newQuote: function(quete) {
+      if (this.quetes.length >= this.maxQuetes) {
+        return alert("Please delete quotes first");
+      }
+      this.quetes.push(quete);
+    },
+    deleteQuote: function(index) {
+      this.quetes.splice(index, 1);
+    }
+  },
+  components: {
+    appHeader: Header,
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ Wonderful Quotes/src/components/Quote.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="panel panel-default">
+            <div class="panel-body quote">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ["quotes"]
+};
+</script>
+
+<style>
+.panel-body {
+  font-family: "Arizonia", sans-serif;
+  font-style: italic;
+  font-size: 24px;
+  color: #b6b6b6;
+}
+.quote {
+  cursor: pointer;
+}
+.quote:hover {
+  background-color: #ffe2e2;
+}
+</style>
