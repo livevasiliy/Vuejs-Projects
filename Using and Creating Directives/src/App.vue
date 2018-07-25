be64288089d12febb4bbc2ee5cf9f8991f34cf30
livevasiliy <vasiliy.mv.obzor@gmail.com>
2018-07-25 15:37:19 +0400

    Добавил новый проект Using and Creating Directives

---
+++ Using and Creating Directives/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives</h1>
+                <p v-highlight:background.delayed="'red'">Global Directives</p>
+                <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 500}">Local Directives</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  directives: {
+    "local-highlight": {
+      bind(el, binding, vnode) {
+        var delay = 0;
+        if (binding.modifiers["delayed"]) {
+          delay = 3000;
+        }
+        if (binding.modifiers["blink"]) {
+          let mainColor = binding.value.mainColor;
+          let secondColor = binding.value.secondColor;
+          let currentColor = mainColor;
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor == secondColor
+                ? (currentColor = mainColor)
+                : (currentColor = secondColor);
+              if (binding.arg == "background") {
+                el.style.backgroundColor = currentColor;
+              } else {
+                el.style.color = currentColor;
+              }
+            }, 1000);
+          }, delay);
+        } else {
+          setTimeout(() => {
+            if (binding.arg == "background") {
+              el.style.backgroundColor = binding.value.mainColor;
+            } else {
+              el.style.color = binding.value.mainColor;
+            }
+          }, binding.value.delay);
+        }
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
