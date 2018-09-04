a71af3ea8baff1d7d3e9f0b148d790d2a084b29b
livevasiliy <vasiliy.mv.obzor@gmail.com>
2018-09-04 13:39:45 +0400

    Add new files

---
+++ Improving your App with Filters and Mixins/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Filters & Mixins</h1>
+                <p>{{ text | toUppercase() | to-lowercase() }}</p>
+                <hr>
+                <button @click="fruits.push('Orange')">Add new item</button>
+                <input type="text" v-model="filterText">
+                <ul>
+                    <li v-for="(fruit, index) in filtredFruits" :key="index">{{ fruit }}</li>
+                </ul>
+                <hr>
+                <app-list></app-list>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import List from "./List.vue";
+import { fruitMixin } from "./fruitMixins.js";
+
+export default {
+  data() {
+    return {
+      text: "Hello"
+    };
+  },
+  filters: {
+    toUppercase(value) {
+      return value.toUpperCase();
+    }
+  },
+  mixins: [fruitMixin],
+  components: {
+    appList: List
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ Improving your App with Filters and Mixins/src/List.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Mixins</h2>
+        <input type="text" v-model="filterText">
+        <ul>
+            <li v-for="(fruit, index) in filtredFruits" :key="index">{{ fruit }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { fruitMixin } from "./fruitMixins.js";
+
+export default {
+  mixins: [fruitMixin]
+};
+</script>
+
+<style>
+</style>
